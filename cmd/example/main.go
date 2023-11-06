38c9869fce7827d28221bcfa36f2ba80fd15ff8f
ne-sachirou <ne-sachirou@hatena.ne.jp>
2023-11-07 00:35:43 +0900

    複数の Server を起動する例

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,36 +2,65 @@ package main
 
 import (
 	"context"
-	_ "errors"
-	"os"
+	"fmt"
 	"time"
 
 	"github.com/ne-sachirou/go-graceful"
-	"golang.org/x/exp/slog"
 )
 
 type ExampleServer struct{}
 
 func (s *ExampleServer) Serve(ctx context.Context) error {
-	slog.InfoCtx(ctx, "serve")
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("example done")
+				return
+			case <-time.After(time.Second):
+				fmt.Println("example working")
+			}
+		}
+	}()
+	fmt.Println("example serve")
 	return nil
-	//return errors.New("can't serve")
 }
 
 func (s *ExampleServer) Shutdown(ctx context.Context) error {
-	slog.InfoCtx(ctx, "shutdown")
+	fmt.Println("example shutdown")
 	return nil
-	//return errors.New("can't shutdown")
+}
+
+type ExampleBlockingServer struct{}
+
+func (s *ExampleBlockingServer) Serve(ctx context.Context) error {
+	fmt.Println("example blocking serve")
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("example blocking done")
+			return nil
+		case <-time.After(time.Second):
+			fmt.Println("example blocking working")
+		}
+	}
+}
+
+func (s *ExampleBlockingServer) Shutdown(ctx context.Context) error {
+	fmt.Println("example blocking shutdown")
+	return nil
 }
 
 func main() {
 	ctx := context.Background()
 	srv := graceful.Servers{
-		Servers: []graceful.Server{&ExampleServer{}},
+		Servers: []graceful.Server{
+			&ExampleServer{},
+			&ExampleBlockingServer{},
+		},
 	}
 	cfg := graceful.Config{ShutdownTimeout: time.Second}
 	if err := srv.Graceful(ctx, cfg); err != nil {
-		slog.ErrorCtx(ctx, err.Error())
-		os.Exit(1)
+		panic(err)
 	}
 }
