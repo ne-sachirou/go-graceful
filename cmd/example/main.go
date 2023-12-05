d08a910ea579c9dd9dc3b8ae3f5caac3a3bb7508
ne-sachirou <ne-sachirou@hatena.ne.jp>
2023-12-05 15:55:28 +0900

    例を、Serve 函数の Context.Done() chan に頼らない様に書き換へ

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -10,14 +11,20 @@ import (
 )
 
 // main loop を go routine の中で回す例。
-type ExampleServer struct{}
+type ExampleServer struct {
+	done       chan struct{}
+	shutdowned chan struct{}
+}
 
 func (s *ExampleServer) Serve(ctx context.Context) error {
+	s.done = make(chan struct{})
+	s.shutdowned = make(chan struct{})
 	go func() {
 		for {
 			select {
-			case <-ctx.Done():
+			case <-s.done:
 				fmt.Println("example done")
+				close(s.shutdowned)
 				return
 			case <-time.After(time.Second):
 				fmt.Println("example working")
@@ -30,19 +37,34 @@ func (s *ExampleServer) Serve(ctx context.Context) error {
 
 func (s *ExampleServer) Shutdown(ctx context.Context) error {
 	fmt.Println("example shutdown")
-	return nil
+	close(s.done)
+	select {
+	case <-s.shutdowned:
+		return nil
+	case <-ctx.Done():
+		if err := ctx.Err(); errors.Is(err, context.DeadlineExceeded) {
+			return context.DeadlineExceeded
+		}
+		return context.Canceled
+	}
 }
 
 // 呼び出し元を blocking して main loop を回す例。
-type ExampleBlockingServer struct{}
+type ExampleBlockingServer struct {
+	done       chan struct{}
+	shutdowned chan struct{}
+}
 
 func (s *ExampleBlockingServer) Serve(ctx context.Context) error {
+	s.done = make(chan struct{})
+	s.shutdowned = make(chan struct{})
 	fmt.Println("example blocking serve")
 	for {
 		select {
-		case <-ctx.Done():
+		case <-s.done:
 			fmt.Println("example blocking done")
-			return context.Canceled
+			close(s.shutdowned)
+			return nil
 		case <-time.After(time.Second):
 			fmt.Println("example blocking working")
 		}
@@ -51,7 +73,16 @@ func (s *ExampleBlockingServer) Serve(ctx context.Context) error {
 
 func (s *ExampleBlockingServer) Shutdown(ctx context.Context) error {
 	fmt.Println("example blocking shutdown")
-	return nil
+	close(s.done)
+	select {
+	case <-s.shutdowned:
+		return nil
+	case <-ctx.Done():
+		if err := ctx.Err(); errors.Is(err, context.DeadlineExceeded) {
+			return context.DeadlineExceeded
+		}
+		return context.Canceled
+	}
 }
 
 func main() {
